6b8c1b7ddbab363369e614a1c32f6a029007d469
MrCocomass <daniel_miranda_apps1ma1718@cev.com>
2019-05-22 22:34:35 +0200

    comit_miercoles

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -12,4 +12,50 @@ use App\Users;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+
+protected function error($code, $message)
+    {
+        $json = ['message' => $message];
+        $json = json_encode($json);
+        return  response($json, $code)->header('Access-Control-Allow-Origin', '*');
+    }
+    
+    protected function success($code, $message, $data = [])
+    {
+    	$json = ['code' => $code, 'message' => $message, 'data' => $data];
+        $json = json_encode($json);
+        return  response($json, 200)->header('Access-Control-Allow-Origin', '*');
+    }
+
+    function createResponse($code, $message, $data = [])
+    {
+        if ($data == null) {
+           $data = (object)[];
+        }
+        return response()->json([
+            'code' => $code,
+            'message' => $message,
+            'data' => $data
+        ]);
+
+    }
+
+    protected function checkLogin($email, $password)
+    {
+        $userSave = Users::where('email', $email)->first();
+        $emailSave = $userSave->email;
+        $passwordSave = $userSave->password;
+        if($emailSave == $email && $passwordSave == $password)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    function is_valid_email($str)
+		{
+  			return (false !== strpos($str, "@") && false !== strpos($str, "."));
+		} 
+
 }
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,30 +3,51 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use \Firebase\JWT\JWT;
+use DB;
 use App\Users;
 
 class UserController extends Controller
 {
     public function register()
     {
-    	
-    	$name = $_POST['name'];
-        $email = $_POST['email'];
-        $password = $_POST['password'];
-        $users = Users::where('email', $email)->get();
 
-		$users = new Users();
-            $users->email = $email;
-            $users->password = $password;
-            $users->name = $name;
-            $users->save();
+    		if (empty($_POST['email']) || empty($_POST['password']) )
+    		{
+    			print('rellenar todos los campos');
+    			exit();
+    		}
 
-        
+    		if(strlen($_POST['password']) < 8 || strlen($_POST['password']) > 12)
+    		{
+                return $this->createResponse(400, 'La contraseña ha de tener entre 8 y 12 caracteres');
+        	}
+
+        	
+    		$name = $_POST['name'];
+        	$email = $_POST['email'];
+        	$password = $_POST['password'];
+        	$users = Users::where('email', $email)->get();
+
+			$users = new Users();
+           		$users->email = $email;
+            	$users->password = $password;
+            	$users->name = $name;
+            	$users->save();
+  
     }
+	
+	public function login()
 
-    public function login()
     {
+    	$email = $_POST['email'];
+            $password = $_POST['password'];
+            $users = Users::where('email', $email)->get();
 
+    	if(self::checkLogin($email, $password)){ 
+
+                $userSave = Users::where('email', $email)->first();
+            }
     }
        
 }
